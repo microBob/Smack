530adc73fb0b1d96b5bb59a10c305c81234e0dcb
Microbob µ <microbob@is88.com>
2017-11-23 15:51:11 -0500

    lesson 8: building AuthService and UserDefaults

--- Smack/Util/Constants.swift
+++ Smack/Util/Constants.swift
@@ -8,7 +8,19 @@
 
 import Foundation
 
+
+typealias CompletionHandler = (_ Success: Bool) -> ()
+
 //MARK: Segues
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND = "unwindToChannel"
+
+//MARK: User Default
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
+
+//MARK: URL constants
+let BASE_URL = "https://smackchatsmack.herokuapp.com/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
